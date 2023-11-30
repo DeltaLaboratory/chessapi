376e40751b469a03f44c675d909f81c566fa9bfe
delta <delta@deltalab.dev>
2023-11-30 11:38:32 +0900

    Wtf LoL

--- main.go
+++ main.go
@@ -18,15 +18,15 @@ func main() {
 
 func setStockfishPermissions() {
 	if err := os.Chmod(fmt.Sprintf("%s/stockfish", os.Getenv("KO_DATA_PATH")), 0777); err != nil {
-		panic(err)
+		fmt.Printf("Failed to chmod stockfish: %s\n", err.Error())
 	}
 
 	// check stockfish executable
 	if stat, err := os.Stat(fmt.Sprintf("%s/stockfish", os.Getenv("KO_DATA_PATH"))); err != nil {
-		panic(err)
+		fmt.Printf("Failed to stat stockfish: %s\n", err.Error())
 	} else {
 		if stat.Mode()&0111 == 0 {
-			panic("Stockfish is not executable")
+			fmt.Printf("Stockfish is not executable\n")
 		}
 
 		if err = exec.Command(fmt.Sprintf("%s/stockfish", os.Getenv("KO_DATA_PATH")), "--help").Run(); err != nil {
